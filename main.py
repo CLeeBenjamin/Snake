2bcae8631513730fbf29ed252091f80487c410f8
Caston Boyd <cboyd@ebrooke.org>
2021-01-15 21:49:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame 
+from pygame.locals import *
+
+
+pygame.init()
+
+#create blank game window 
+screen_width = 400 
+screen_height = 400 
+
+#gamewindow 
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Snake')
+
+#setup loop with exit event handler 
+run = True 
+
+#colors 
+bg = (255, 200,150)
+
+def draw_screen():
+  screen.fill(bg)
+
+while run: 
+
+  draw_screen()
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT: 
+      run = False
+
+  pygame.display.update()
+
+
+pygame.quit()
+
